090b9bba1d1397e8bee2b1ff8e25d6ebbf4321fc
KangSeongKwan <kang53497@naver.com>
2023-03-20 21:18:20 +0900

    Daily Study and Some Bugs-2023-03-20

--- main.js
+++ main.js
@@ -40,50 +40,16 @@ app.get('/', function(request, response) {
   <p>${description}</p>
   <img src="/images/hello.jpg" style="width:300px; display:block margin-top:10px;">
   `, `
-  <a href="/create">Create</a>`);
+  <a href="/topic/create">Create</a>`);
   response.send(html);
 });
 
-// :pageId 부분은 /page 아래에 인수를 주면 그 값을 request.params에 저장함.
-app.get('/page/:pageId', function(request, response, next) {
-  // 정확하게는 request.params에는 /page/아래의 모든 :변수명 값을 저장함
-  var filterId = path.parse(request.params.pageId).base;
-  var list = template.list(request.list);
-  fs.readFile(`./data/${filterId}`, 'utf-8', function(err, description){
-    if(err){
-      next(err);
-      // next('route'); 는 정상처리임
-    }
-    else {
-      var title = request.params.pageId;
-      var sanitizedTitle = sanitizeHTML(title);
-      var sanitizedDescription = sanitizeHTML(description, {
-        allowedTags:['h1']
-      });
-      var html = template.HTML(title, list, `<h2>${sanitizedTitle}</h2>
-      <p>${sanitizedDescription}</p>`,
-      `
-      <a href= "/create">Create</a> 
-      <a href = "/update/${sanitizedTitle}">Update</a>    
-      <form action="/delete_process" method="post">
-        <input type="hidden" name="id" value="${sanitizedTitle}">
-        <input type="submit" value="delete">
-      </form>`);
-      // QueryString을 사용하는 것은 Get 방식
-      /* 삭제 버튼을 Link로 구현하는 것은 대단히 잘못된일(소스를 조작할 수 있기 때문)
-      <a href = "/delete?id=${title}">Delete</a> 
-      */
-      response.send(html);
-    }
-  
-  });
-});
-
-app.get('/create', function(request, response){
+// 여기서 순서가 중요해지게됨. /topic 아래에서는 우선순위가 create가 먼저 실행되는 부분
+app.get('/topic/create', function(request, response){
   var title = 'WEB - create';
   var list = template.list(request.list);
   var html = template.HTML(title, list, `
-    <form action="/create_process" method="post">
+    <form action="/topic/create_process" method="post">
     <p><input type="text" name="title" placeholder="title"></p>
     <p>
         <textarea name="description" placeholder="description"></textarea>
@@ -94,16 +60,18 @@ app.get('/create', function(request, response){
     </form>
   `);
   response.send(html);
-
 });
 
-app.post('/create_process', function(request, response){
+app.post('/topic/create_process', function(request, response){
   var post = request.body;
   var title = post.title;
   var description = post.description;
   fs.writeFile(`./data/${title}`, description, 'utf8', function(err){
+    response.redirect(`/topic/${title}`);
+    /*
     response.writeHead(302, {Location: `/?id=${title}`});
     response.end();
+    */
   });
 
   /* body-parser의 urlencode 미들웨어를 활용해 리팩토링
@@ -123,13 +91,13 @@ app.post('/create_process', function(request, response){
   */
 });
 
-app.get('/update/:pageId', function(request, response){
+app.get('/topic/update/:pageId', function(request, response){
   var filterId = path.parse(request.params.pageId).base;
   fs.readFile(`./data/${filterId}`, 'utf-8', function(err, description){
     var title = request.params.pageId;
     var list = template.list(request.list);
     var html = template.HTML(title, list, `
-      <form action="/update_process" method="post">
+      <form action="/topic/update_process" method="post">
       <input type="hidden" name="id" value="${title}">
       <p><input type="text" name="title" 
       placeholder="title" value="${title}"></p>
@@ -143,13 +111,13 @@ app.get('/update/:pageId', function(request, response){
       </form>
     `,
     `
-    <a href="/create">Create</a> 
+    <a href="/topic/create">Create</a> 
     <a href = "/update/${title}">Update</a>`);
     response.send(html);
   });
 });
 
-app.post('/update_process', function(request, response){
+app.post('/topic/update_process', function(request, response){
   var post = request.body;
   var id = post.id;
   var filterId = path.parse(id).base;
@@ -157,12 +125,12 @@ app.post('/update_process', function(request, response){
   var description = post.description;
   fs.rename(`./data/${filterId}`, `./data/${title}`, function(error){
     fs.writeFile(`./data/${title}`, description, 'utf8', function(err){
-      response.redirect(`/?id=${title}`)
+      response.redirect(`/topic/${title}`)
     });
   });
 });
 
-app.post('/delete_process', function(request,response){
+app.post('/topic/delete_process', function(request,response){
   var post = request.body;
   var id = post.id;
   var filterId = path.parse(id).base;
@@ -172,6 +140,41 @@ app.post('/delete_process', function(request,response){
     response.writeHead(302, {Location: '/'});
     response.end();
     */
+  });
+});
+
+// :pageId 부분은 /page 아래에 인수를 주면 그 값을 request.params에 저장함.
+app.get('/topic/:pageId', function(request, response, next) {
+  // 정확하게는 request.params에는 /page/아래의 모든 :변수명 값을 저장함
+  var filterId = path.parse(request.params.pageId).base;
+  var list = template.list(request.list);
+  fs.readFile(`./data/${filterId}`, 'utf-8', function(err, description){
+    if(err){
+      next(err);
+      // next('route'); 는 정상처리임
+    }
+    else {
+      var title = request.params.pageId;
+      var sanitizedTitle = sanitizeHTML(title);
+      var sanitizedDescription = sanitizeHTML(description, {
+        allowedTags:['h1']
+      });
+      var html = template.HTML(title, list, `<h2>${sanitizedTitle}</h2>
+      <p>${sanitizedDescription}</p>`,
+      `
+      <a href= "/topic/create">Create</a> 
+      <a href = "/topic/update/${sanitizedTitle}">Update</a>    
+      <form action="/topic/delete_process" method="post">
+        <input type="hidden" name="id" value="${sanitizedTitle}">
+        <input type="submit" value="delete">
+      </form>`);
+      // QueryString을 사용하는 것은 Get 방식
+      /* 삭제 버튼을 Link로 구현하는 것은 대단히 잘못된일(소스를 조작할 수 있기 때문)
+      <a href = "/delete?id=${title}">Delete</a> 
+      */
+      response.send(html);
+    }
+  
   });
 });
 
